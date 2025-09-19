98c0df4aa5ce3933f5f5cb00cce80c8fe73888a6
Vitor1s <vitor.severo@sou.unifeob.edu.br>
2025-09-19 17:38:52 -0300

    Initial steps in python

---
+++ first_steps.py
@@ -0,0 +1,11 @@
+#Tipos em python na prática. 
+print(1 + 10 )  ##int.
+print( 1.5 + 2.30)  ##float.
+print("Hello 2" + " World 5")  ##str.
+print(True + False) #Aqui ele retorna 1 pq True = 1 e False = 0 / porque  True + False = 1 + 0 = 1 e seria 1 é True
+
+
+int()
+str()
+float()
+bool()
